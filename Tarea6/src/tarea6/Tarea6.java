6ff2f8ec2f45b856299e5dbf564c2914d83bd5db
jazmin08ibieta <72178556+jazmin08ibieta@users.noreply.github.com>
2021-06-21 19:19:44 -0500

    Add files via upload

---
+++ Tarea6/src/tarea6/Tarea6.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea6;
+
+import ico.fes.persona.Alumno;
+import ico.fes.persona.Persona;
+
+/**
+ *
+ * @author ibieta
+ */
+public class Tarea6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //prueba de herencia clase persona.
+        
+        Alumno per1 = new Alumno("318043371", "ICO", 2, "Jazmin Ibieta", 19, 2, 1.54f);
+        
+        System.out.println(per1.toString());
+        
+      
+    }
+    
+}
